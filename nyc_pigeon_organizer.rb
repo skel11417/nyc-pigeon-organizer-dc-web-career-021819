f46a7bb9a65499a9c4385b6357a52fd5bca59b76
Sean Kelly <shonkelli@gmail.com>
2019-02-06 19:28:27 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,7 +2,7 @@ require 'pry'
 def nyc_pigeon_organizer(data)
   # write your code here!
   output = {}
-  binding.pry
+  # binding.pry
   data.each do |key, value|
     value.each do |attribute, names|
       names.each do |name|
