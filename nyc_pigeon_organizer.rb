5f45c60a45bfbc779c3ea2f4d90c03514e45a85b
Sean Kelly <shonkelli@gmail.com>
2019-02-06 19:15:58 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,6 +2,9 @@ require 'pry'
 def nyc_pigeon_organizer(data)
   # write your code here!
   output = {}
+  colors = data[:color]
+  genders = data[:gender]
+  location = data[:lives]
   binding.pry
   
 end
