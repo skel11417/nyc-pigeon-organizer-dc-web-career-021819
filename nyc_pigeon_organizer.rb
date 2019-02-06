67fffcb45b458f77507b1a5925cf269eba8a72bc
Sean Kelly <shonkelli@gmail.com>
2019-02-06 19:41:12 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,15 @@
+require 'pry'
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  output = {}
+  data.each do |key, value|
+    value.each do |attribute, names|
+      names.each do |name|
+        if output[name].nil?
+          output[name] = {:color => [], :gender => [], :lives => []}
+        end
+        output[name][key] << attribute.to_s
+      end
+    end
+  end
+  output
 end
