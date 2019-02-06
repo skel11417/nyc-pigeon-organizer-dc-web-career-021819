db61a081cf7e74588af54f53d6e655b80981f783
Sean Kelly <shonkelli@gmail.com>
2019-02-06 19:20:04 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,7 +5,6 @@ def nyc_pigeon_organizer(data)
   colors = data[:color]
   genders = data[:gender]
   location = data[:lives]
-  # binding.pry
   data.each do |key, value|
     value.each do |name|
       output[name]
