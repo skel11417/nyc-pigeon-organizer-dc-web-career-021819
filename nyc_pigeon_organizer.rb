133174340ea0601569cbfca3a43ffc2974806897
Sean Kelly <shonkelli@gmail.com>
2019-02-06 19:30:27 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -6,7 +6,7 @@ def nyc_pigeon_organizer(data)
   data.each do |key, value|
     value.each do |attribute, names|
       names.each do |name|
-        output[name] = {}
+        output[name] =
       end
     end
   end
